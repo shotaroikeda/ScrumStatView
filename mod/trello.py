52843ce4bdcd9c801810e3b7e5a233534d2b0d3f
shotaroikeda <ikeda.shot@gmail.com>
2015-09-15 15:32:01 -0500

    Basic trello object

--- mod/trello.py
+++ mod/trello.py
@@ -1,1 +1,66 @@
 import requests
+from requests_oauthlib import OAuth1
+import selenium
+from os.path import isfile
+
+
+class TrelloBoard(object):
+    def __init__(self, key=None, secret_key=None, v=False):
+        self.key = key
+        self.secret_key = secret_key
+
+        if isfile('token.cache'):
+            if v:
+                print "Attempting to read previously created token..."
+
+            with open('token.cache', 'r') as f:
+                self.token = f.read()
+
+        else:
+            if v:
+                print "Generating a new token..."
+
+            self.token = self.generate_token()
+
+    def generate_url(self):
+        return "https://api.trello.com/1/boards/" + self.key
+
+    def generate_token(self):
+        auth = OAuth1(self.key, self.secret_key)
+        u = 'https://trello.com/1/OAuthGetRequestToken'
+        r = requests.get(u, auth=auth)
+
+        b = r.text
+        print b
+
+        'https://trello.com/1/OAuthAuthorizeToken'
+
+        with open('token.cache', 'w') as f:
+            f.write(b)
+
+        r.close()
+        return b
+
+    def get_member_record(self):
+        u = "https://trello.com/1/members/me?key=%s&token=%s" % (
+            self.key, self.token)
+
+        print u
+
+        return self.obtain_json_obj(u)
+
+    def get_open_boards(self):
+        u = "https://trello.com/1/members/my/boards?key=%s&token=%s" % (
+            self.key, self.token)
+        return self.obtain_json_obj(u)
+
+    def get_pinned_boards(self):
+        u = "https://trello.com/1/members/my/boards/pinned?key=%s&token=%s" % (
+            self.key, self.token)
+        return self.obtain_json_obj(u)
+
+    def obtain_json_obj(self, url):
+        r = requests.get(url, stream=True)
+        obj = r.raw.read()
+        r.close()
+        return obj
--- tests/test.py
+++ tests/test.py
@@ -2,6 +2,10 @@ from mod import trello
 from mod import csv
 
 
+TRELLO_KEY = "68917fe255b5ff33b40531dbd5d7098c"
+TRELLO_SECRET_KEY = "463726887fc785951839205809fd1a5a98fdf65a2af97fe1bbaa5ad6d130c4c4"
+
+
 def test_trello():
     pass
 
